5fa02823687a4dcf1b76d7dce134022f85b895cf
Guy Arad <guy@placer.io>
2021-10-19 16:19:03 +0300

    Repro for celery step-into bug PY-48388

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -3,10 +3,16 @@ from time import sleep
 from celery_tasks.celery_app import app
 
 
+def foo(s):
+    print(f'inside foo doing some stuff: {s}')
+
+
 @app.task
 def do_sum(x, y) -> int:
     sleep(3)
     # Put a breakpoint inside here when testing the issue. The task will be received but never progresses.
     # Remove the breakpoint and task completes correctly.
     print('Calculating...')
+    # this is for testing step-into issue. Put a breakpoint here and attempt to step-into `foo`
+    foo(f'{x} + {y}')
     return sum([x, y])
